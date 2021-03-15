b05d3745551e3b50d6d5c14b855a6ad3d4bf713e
HoshinoTented <limbolrain@gmail.com>
2021-03-15 10:39:08 +0000

    common test for  `f32_add`

--- src/soft_f32/soft_f32_add.rs
+++ src/soft_f32/soft_f32_add.rs
@@ -165,14 +165,17 @@ mod tests {
         // -Inf + -1 = -Inf
         assert_eq!(crate::soft_f32::f32_add(0xFF800000, 0x3F800000), 0xFF800000);
 
+        // common tests about NaN
+        crate::soft_f32::test_util::test_nan(super::f32_add);
+
         // NaN + 1 = NaN
         assert_eq!(crate::soft_f32::f32_add(0xFFFFFFFF, 0x3F800000), 0xFFFFFFFF);
 
         // NaN + -1 = NaN
-        assert_eq!(crate::soft_f32::f32_is_nan(crate::soft_f32::f32_add(0xFFFFFFFF, 0x3F800000)), true);
+        assert_eq!(crate::soft_f32::f32_is_nan(crate::soft_f32::f32_add(0xFFFFFFFF, 0x3F800000)), true);        // what is it? 1 == -1?
 
         // NaN + Inf = NaN
-        assert_eq!(crate::soft_f32::f32_is_nan(crate::soft_f32::f32_add(0xFFFFFFFF, 0x7F800000)), true);
+        assert_eq!(crate::soft_f32::f32_is_nan(crate::soft_f32::f32_add(0xFFFFFFFF, 0x7F800000)), true);        // why not use assert!()
 
         // NaN + -Inf = NaN
         assert_eq!(crate::soft_f32::f32_is_nan(crate::soft_f32::f32_add(0xFFFFFFFF, 0xFF800000)), true);
--- src/soft_f32/test_util.rs
+++ src/soft_f32/test_util.rs
@@ -1,2 +1,22 @@
-pub(crate) const INF: u32 = 0x7F800000;
+use crate::soft_f32::f32_is_nan;
+
+pub(crate) const INF: u32 = 0x7F800000;         // Consider making they public
 pub(crate) const NEG_INF: u32 = 0xFF800000;
+pub(crate) const ONE: u32 = 0x3F800000;
+pub(crate) const NEG_ONE: u32 = 0xBF800000;
+
+pub fn test_nan<F>(f: F)
+    where
+        F: Fn(u32, u32) -> u32 {
+
+    // NaN `f` ± 1 = NaN
+    assert!(f32_is_nan(f(0xFFFFFFFF, ONE)));
+    assert!(f32_is_nan(f(0xFFFFFFFF, NEG_ONE)));
+
+    // NaN `f` ± INF = NaN
+    assert!(f32_is_nan(f(0xFFFFFFFF, INF)));
+    assert!(f32_is_nan(f(0xFFFFFFFF, NEG_INF)));
+
+    // NaN `f` NaN = NaN
+    assert!(f32_is_nan(f(0xFFFFF114, 0xFFFFF514)));
+}
